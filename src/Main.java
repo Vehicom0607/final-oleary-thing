6919a84c3cbf16005b85ee9d04f7c9c8145cf583
elvis <osmium15elvis@gmail.com>
2023-02-06 01:29:33 -0500

    fuck me

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import me.tongfei.progressbar.ProgressBar;
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) {
@@ -24,15 +28,43 @@ public class Main {
             round_list.add(getRounds(useful_matchup_list, (player_count/2) - 1, player_count, i));
         }
 
-        System.out.println("round_list = " + round_list);
-
         List<List<int[]>> bracket_list = new ArrayList<>();
         bracket_list = getBracket(round_list, player_count);
 
         System.out.println("bracket_list = " + bracket_list.size());
 
-        System.out.println(checkBracketValidity(bracket_list.get(0)));
+        long tracker = 0;
+        List<List<int[]>> solutions = new ArrayList<>();
+        for (List<int[]> round : bracket_list) {
+            if (checkBracketValidity(round)) {
+                solutions.add(round);
+                System.out.println("Found");
+            }
+            tracker++;
+        }
 
+
+        // Print out in a file
+        try {
+            FileWriter write = new FileWriter(String.format("Solutions with %d.txt", player_count));
+            for (List<int[]> solution : solutions) {
+                write.write("SOLUTION: + \n");
+                for (int[] solution_match: solution) {
+                    write.write(Arrays.toString(solution_match) + "\n");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("rip");
+            e.printStackTrace();
+        }
+
+
+
+        // Get Runtime
+        long end = System.currentTimeMillis();
+
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time is " + formatter.format((end - start) / 1000d) + " seconds");
 
 
     }
@@ -68,15 +100,29 @@ public class Main {
     }
 
     public static List<List<int[]>> getBracket(List<List<int[]>> rounds, int players) {
-        Iterator<int[]> pairing_iterator = CombinatoricsUtils.combinationsIterator(rounds.get(0).size(), players - 1);
+        int[] counter = new int[players - 1];
+        int round_len = rounds.get(0).size();
         List<List<int[]>> comb = new ArrayList<>();
-        while (pairing_iterator.hasNext()) {
-            final int[] combination = pairing_iterator.next();
+        while (counter[0] < round_len) {
             List<int[]> temp_bracket = new ArrayList<>();
             for (int i = 0; i < rounds.size(); i++) {
-                temp_bracket.add(rounds.get(i).get(combination[i]));
+                temp_bracket.add(rounds.get(i).get(counter[i]));
             }
             comb.add(temp_bracket);
+
+            if (counter[counter.length - 1] != round_len - 1) {
+                counter[counter.length - 1]++;
+            } else {
+                counter[counter.length - 1] = 0;
+                counter[counter.length - 2]++;
+                for (int i = counter.length - 2; i > 0; i--) {
+                    if (counter[i] == round_len) {
+                        counter[i] = 0;
+                        counter[i - 1]++;
+                    }
+                }
+            }
+
         }
         return comb;
     }
@@ -105,39 +151,38 @@ public class Main {
         int len = bracket.get(0).length;
         int[][] teammates = new int[len][len];
         int[][] opponents = new int[len][len];
-        for (int i =  0; i < bracket.get(0).length; i++) {
-            for (int k = 0; k < len; k++) {
-                teammates[k][k]++;
-                opponents[k][k] += 2;
-            }
-            for (int[] round: bracket) {
-                for (int j = 0; j < round.length; j++) {
-                    if (j % 4 == 0) {
-                        teammates[j][round[j+1]]++;
-                        opponents[j][round[j+2]]++;
-                        opponents[j][round[j+3]]++;
-                    }
-                    if (j % 4 == 1) {
-                        teammates[j][round[j-1]]++;
-                        opponents[j][round[j+1]]++;
-                        opponents[j][round[j+2]]++;
-                    }
-                    if (j % 4 == 2) {
-                        teammates[j][round[j+1]]++;
-                        opponents[j][round[j-1]]++;
-                        opponents[j][round[j-2]]++;
-                    }
-                    if (j % 4 == 3) {
-                        teammates[j][round[j - 1]]++;
-                        opponents[j][round[j - 2]]++;
-                        opponents[j][round[j - 3]]++;
-                    }
+        for (int k = 0; k < len; k++) {
+            teammates[k][k]++;
+            opponents[k][k] += 2;
+        }
+        for (int[] round: bracket) {
+            for (int j = 0; j < round.length; j++) {
+                if (j % 2 == 0) {
+                    teammates[round[j]][round[j+1]]++;
+                } else {
+                    teammates[round[j]][round[j-1]]++;
+                }
+                if (j % 4 == 0 || j % 4 == 1) {
+                    opponents[round[j]][round[j+2 - (j%2)]]++;
+                    opponents[round[j]][round[j+3 - (j%2)]]++;
+                } else {
+                    opponents[round[j]][round[j-1 - (j%2)]]++;
+                    opponents[round[j]][round[j-2 - (j%2)]]++;
                 }
             }
-
         }
 
-        return false;
+        // Verify
+
+        if (Arrays.stream(teammates).flatMapToInt(Arrays::stream).distinct().count() == 1) {
+            //if (Arrays.stream(opponents).flatMapToInt(Arrays::stream).distinct().count() == 1) {
+                return true;
+            //} else {
+            //    return false;
+            //}
+        } else {
+            return false;
+        }
     }
 
 }
